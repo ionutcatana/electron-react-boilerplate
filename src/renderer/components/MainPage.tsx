a136a40abbd9203ee29bd09a9b998a5ea9a146d4
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-05-09 14:49:41 +0300

    add skeleton of a sort of project thing

---
+++ src/renderer/components/MainPage.tsx
@@ -0,0 +1,73 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import "./styles/temp.css";
+
+export function MainPage() {
+  // state hooks
+  const [apiUrl, setApiUrl] = useState("");
+  const [httpVerb, setHttpVerb] = useState("");
+  const [requestText, setRequestText] = useState("");
+  const [responseText, setResponseText] = useState("");
+
+  // handler functions
+  function handleApiUrlChange(e: ChangeEvent<HTMLInputElement>): void {
+    const value = e.target.value;
+    console.log(value);
+    setApiUrl(value);
+  }
+
+  function handleHttpVerbChange(e: ChangeEvent<HTMLSelectElement>): void {
+    const value = e.target.value;
+    console.log(value);
+    setHttpVerb(value);
+  }
+
+  function handleRequestTextChange(e: ChangeEvent<HTMLTextAreaElement>): void {
+    const value = e.target.value;
+    console.log(value);
+    setRequestText(value);
+  }
+
+  // api interaction
+  function sendRequest(apiUrl: string, httpVerb: string, requestBody: string): void {
+    // handle...
+    setResponseText("Your request has been received!");
+  }
+
+  // document structure
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+
+      {/* settings */}
+      <div className="settings">
+
+        {/* http verb */}
+        <label htmlFor="http-verb">Select an HTTP verb: </label>
+        <select name="method-selector" id="method-selector"
+          value={httpVerb}
+          onChange={(e) => handleHttpVerbChange(e)}
+        >
+          <option value="">Please select a value</option>
+          <option value="post">POST</option>
+          <option value="get">GET</option>
+          <option value="patch">PATCH</option>
+          <option value="delete">DELETE</option>
+        </select>
+
+        {/* url of the api */}
+        <label htmlFor="api-url">API URL: </label>
+        <input type="text" name="api-url" id="api-url-input" value={apiUrl} onChange={(e) => handleApiUrlChange(e)}/>
+
+      </div>
+
+      {/* display */}
+      <div className="split-vertical">
+        <textarea placeholder="Your request" value={requestText} onChange={(e) => handleRequestTextChange(e)}></textarea>
+        <textarea placeholder="API response" defaultValue={responseText}></textarea>
+      </div>
+
+      {/* controls */}
+      <button onClick={() => sendRequest(apiUrl, httpVerb, requestText)}>Send</button>
+    </div>
+  );
+}
