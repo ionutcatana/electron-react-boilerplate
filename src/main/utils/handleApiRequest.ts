5cf0027cd37cf0f800795c9eed168bd5d02ed63b
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-05-10 17:52:39 +0300

    add dependency for API requests

--- src/main/utils/handleApiRequest.ts
+++ src/main/utils/handleApiRequest.ts
@@ -1,10 +1,4 @@
 import { net } from "electron"
 
 export default function handleApiRequest(method: string, path: string, isJson: boolean, message: string | object): void {
-    const request = net.request({
-        method,
-        protocol: 'http:',
-        hostname: 'localhost:3000',
-        path
-    })
 }
